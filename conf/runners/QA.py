5e050a92eb4d2469fb5a5613b2ba41d8f6d8da92
burlamix <burlami@gmail.com>
2018-12-04 16:31:18 +0100

    scrpt for all

---
+++ conf/runners/QA.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+
+import os
+
+
+print("Start running your script....\n")
+os.system("ls")
+os.chdir("../../../../SmartApp.QA")
+os.system("ls")
+
+os.system("python fc.py &")
+
+#for windos
+os.system("start /b python fc.py &")
+
+print("Your script is running! ")
